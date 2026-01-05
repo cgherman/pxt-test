f83406d54af968c5ef895392be3bbd2bb2231a1c
Chris <cgherman@gmail.com>
2026-01-05 15:38:14 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
- 
+/**
+* This is a Test Namespace
+*/
+//% weight=70 icon="\uf185" color=#EC7505
+namespace testing {
+    /**
+     * Check if your number is greater than zero.
+     */
+    //% blockId=testing_check block="pick an %inputValue"
+    //% help=testing/check
+    export function TestingCheck(inputValue: number): boolean {
+        return inputValue > 0
+    }
+
+    /**
+     * Get the opposite of your number
+     */
+    //% blockId=testing_flip block="Flip your %inputValue"
+    //% help=testing/flip
+    export function TestingFlip(inputValue: number): number {
+        return -inputValue;
+    }
+}
